7ec72f86720fe6ffce100b1766d2544b3c4a059c
syonik <32883283+syonik@users.noreply.github.com>
2022-12-16 11:44:05 +0200

    variable num args in action

--- functions_namespaces_and_scope/variable_num_args.py
+++ functions_namespaces_and_scope/variable_num_args.py
@@ -1,6 +1,33 @@
+tables = {
+    1: {
+        'name': 'Jiho',
+        'vip_status': False,
+        'order': 'Orange Juice, Apple Juice'
+    },
+    2: {},
+    3: {},
+    4: {},
+    5: {},
+    6: {},
+    7: {},
+}
+print(tables)
+
+
+def assign_table(table_number, name, vip_status=False):
+    tables[table_number]['name'] = name
+    tables[table_number]['vip_status'] = vip_status
+    tables[table_number]['order'] = ''
+
 # Write your code below:
-def print_order(*order_items):
-    print(order_items)
 
 
-print_order('Orange Juice', 'Apple Juice', 'Scrambled Eggs', 'Pancakes')
+def assign_and_print_order(table_number, *order_items):
+    tables[table_number]['order'] = order_items
+    for order_item in order_items:
+        print(order_item)
+
+
+assign_table(2, 'Arwa', True)
+assign_and_print_order(2, 'Steak', 'Seabass', 'Wine Bottle')
+print(tables)
